6b6ff7c430fd781e84c27c0d58ca75e127b0cfb8
joel9vvs <123255593+joel9vvs@users.noreply.github.com>
2023-01-22 10:53:05 +0000

    test

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##
+## 
+##
+
+## maintained by: hkdb <hkdb@3df.io>
+echo "hiii $1"
+
+
+echo -e "\n"
+read -p 'Enter  container name (eg: dbrave-spring ): ' CONTAINER_NAME
+export HOME="ubbe"
+sed  's|HOME|'$HOME'|g; s|BRAVED|'$CONTAINER_NAME'|g'  dBrave.desktop.skel > $CONTAINER_NAME.desktop
+
+echo -e "\n"
+read -p 'Would you like to add a shortcut to Gnome Launcher? (Y/n): ' GL
